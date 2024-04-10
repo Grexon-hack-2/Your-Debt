b89ac9c19ec10ba6d0975520482dde15257648c9
Grexon-hack-2 <jsarabialugo2306@gmail.com>
2024-04-10 17:28:48 -0500

    task20240410-1728 - ajustes en persistencia de modo oscuro

--- src/Guards/AuthorizatedGuard.ts
+++ src/Guards/AuthorizatedGuard.ts
@@ -1,13 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from 'src/Utils/Auth.service';
+import { PersistenceService } from 'src/Utils/Persistence.service';
 
 export const AuthGuard: CanActivateFn = () => {
-
+    const keyThemeColor: string = "color-Theme";
     const authService  = inject(AuthService);
     const router$ = inject(Router);
+    const persistence = inject(PersistenceService);
 
     if(authService.isAuthenticated()){
+        if(persistence.get(keyThemeColor) !== null && persistence.get(keyThemeColor) === 'dark'){
+            document.body.classList.toggle('dark', true);
+        }
+
         return true;
     }
     else {
